937b9cb59f114446c34ea9c723dd618f6a1c4a5d
zhouxiao <zhouxiao@npn.sg>
2024-05-29 15:37:27 +0800

    add assets

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/'])
+const isPublicRoute = createRouteMatcher(['/', '/learn'])
 
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
