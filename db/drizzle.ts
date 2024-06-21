96b9992b701597cdc64e3c32dab107923250b1a3
filway <filway@126.com>
2024-06-21 14:10:00 +0800

    User Progress

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/mysql2'
 import mysql from 'mysql2/promise'
 import * as schema from './schema'
 
-export const connection = await mysql.createConnection({
+export const connection = mysql.createPool({
   uri: process.env.DATABASE_URL,
 })
 
--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,33 @@
-import { mysqlTable, serial, varchar } from 'drizzle-orm/mysql-core'
+import { relations } from 'drizzle-orm'
+import { mysqlTable, serial, varchar, int, bigint } from 'drizzle-orm/mysql-core'
 
 export const courses = mysqlTable('courses', {
   id: serial('id').primaryKey(),
   title: varchar('title', { length: 255 }).notNull(),
   imageSrc: varchar('image_src', { length: 255 }).notNull(),
 })
+
+export const coursesRelations = relations(courses, ({ many }) => ({
+  userProgress: many(userProgress),
+}))
+
+export const userProgress = mysqlTable('user_progress', {
+  userId: varchar('user_id', { length: 255 }).primaryKey(),
+  userName: varchar('user_name', { length: 255 }).notNull().default('User'),
+  userImageSrc: varchar('user_image_src', { length: 255 }).notNull().default('/mascot.svg'),
+  activeCourseId: bigint('active_course_id', {
+    mode: 'number',
+    unsigned: true,
+  }).references(() => courses.id, {
+    onDelete: 'cascade',
+  }),
+  hearts: int('hearts').notNull().default(5),
+  points: int('points').notNull().default(0),
+})
+
+export const userProgressRelations = relations(userProgress, ({ one }) => ({
+  activeCourse: one(courses, {
+    fields: [userProgress.activeCourseId],
+    references: [courses.id],
+  }),
+}))
