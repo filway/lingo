c9dff7ad886ff25314b228b306053ad142e8ae18
zxxiaoxiao <filway@126.com>
2025-01-22 01:05:15 +0800

    Units

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -14,6 +14,11 @@ const main = async () => {
 
     await db.delete(schema.courses)
     await db.delete(schema.userProgress)
+    await db.delete(schema.units)
+    await db.delete(schema.lessons)
+    await db.delete(schema.challenges)
+    await db.delete(schema.challengeOptions)
+    await db.delete(schema.challengeProgress)
 
     await db.insert(schema.courses).values([
       {
@@ -35,6 +40,62 @@ const main = async () => {
         id: 4,
         title: 'Italian',
         imageSrc: '/it.svg',
+      },
+    ])
+
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        courseId: 1, // Spanish
+        title: 'Unit 1',
+        description: 'Learn the basics of Spanish',
+        order: 1,
+      },
+    ])
+
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        unitId: 1,
+        title: 'Nouns',
+        order: 1,
+      },
+    ])
+
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        lessonId: 1,
+        type: 'SELECT',
+        order: 1,
+        question: 'Which one of these is the "the man"?',
+      },
+    ])
+
+    await db.insert(schema.challengeOptions).values([
+      {
+        id: 1,
+        challengeId: 1,
+        imageSrc: '/man.svg',
+        text: 'el hombre',
+        correct: true,
+        audioSrc: '/es_man.mp3',
+      },
+      {
+        id: 2,
+        challengeId: 1,
+        imageSrc: '/woman.svg',
+        text: 'la mujer',
+        correct: false,
+        audioSrc: '/es_woman.mp3',
+      },
+      {
+        id: 3,
+        challengeId: 1,
+        imageSrc: '/robot.svg',
+        text: 'el robot',
+        correct: false,
+        audioSrc: '/es_robot.mp3',
       },
     ])
 
