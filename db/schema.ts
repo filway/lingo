3d3a9afd2393e6fc02eb76aa856ff18ec236717a
zhouxiao <zhouxiao@npn.sg>
2025-01-21 16:34:12 +0800

    Schema

--- db/schema.ts
+++ db/schema.ts
@@ -5,6 +5,8 @@ import {
   varchar,
   int,
   bigint,
+  mysqlEnum,
+  boolean,
 } from 'drizzle-orm/mysql-core'
 
 export const courses = mysqlTable('courses', {
@@ -55,12 +57,89 @@ export const lessons = mysqlTable('lessons', {
   order: int('order').notNull(),
 })
 
-export const lessonsRelations = relations(lessons, ({ one }) => ({
+export const lessonsRelations = relations(lessons, ({ one, many }) => ({
   unit: one(units, {
     fields: [lessons.unitId],
     references: [units.id],
   }),
+  challenges: many(challenges),
 }))
+
+export const challengesEnum = mysqlEnum('type', ['SELECT', 'ASSIST'])
+
+export const challenges = mysqlTable('challenges', {
+  id: serial('id').primaryKey(),
+  lessonId: bigint('lesson_id', {
+    mode: 'number',
+    unsigned: true,
+  })
+    .references(() => lessons.id, {
+      onDelete: 'cascade',
+    })
+    .notNull(),
+  type: challengesEnum.notNull(),
+  question: varchar('question', { length: 255 }).notNull(),
+  order: int('order').notNull(),
+})
+
+export const challengesRelations = relations(challenges, ({ one, many }) => ({
+  lesson: one(lessons, {
+    fields: [challenges.lessonId],
+    references: [lessons.id],
+  }),
+  challengeOptions: many(challengeOptions),
+  challengeProgress: many(challengeProgress),
+}))
+
+export const challengeOptions = mysqlTable('challenge_options', {
+  id: serial('id').primaryKey(),
+  challengeId: bigint('challenge_id', {
+    mode: 'number',
+    unsigned: true,
+  })
+    .references(() => challenges.id, {
+      onDelete: 'cascade',
+    })
+    .notNull(),
+  text: varchar('text', { length: 255 }).notNull(),
+  correct: boolean('correct').notNull(),
+  imageSrc: varchar('image_src', { length: 255 }),
+  audioSrc: varchar('audio_src', { length: 255 }),
+})
+
+export const challengeOptionsRelations = relations(
+  challengeOptions,
+  ({ one }) => ({
+    challenge: one(challenges, {
+      fields: [challengeOptions.challengeId],
+      references: [challenges.id],
+    }),
+  })
+)
+
+export const challengeProgress = mysqlTable('challenge_progress', {
+  id: serial('id').primaryKey(),
+  userId: varchar('user_id', { length: 255 }).notNull(),
+  challengeId: bigint('challenge_id', {
+    mode: 'number',
+    unsigned: true,
+  })
+    .references(() => challenges.id, {
+      onDelete: 'cascade',
+    })
+    .notNull(),
+  completed: boolean('completed').notNull().default(false),
+})
+
+export const challengeProgressRelations = relations(
+  challengeProgress,
+  ({ one }) => ({
+    challenge: one(challenges, {
+      fields: [challengeProgress.challengeId],
+      references: [challenges.id],
+    }),
+  })
+)
 
 export const userProgress = mysqlTable('user_progress', {
   userId: varchar('user_id', { length: 255 }).primaryKey(),
