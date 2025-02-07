0ed07e976a9ad33442d935b949f6e33ee6ca2aff
zhouxiao <zhouxiao@npn.sg>
2025-02-07 17:21:46 +0800

    Details

--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -2,15 +2,21 @@ import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import { InfinityIcon } from 'lucide-react'
+import { courses } from '@/db/schema'
 
 type Props = {
-  activeCourse: { imageSrc: string; title: string } // TODO: Replace with DB types
+  activeCourse: typeof courses.$inferSelect
   hearts: number
   points: number
   hasActiveSubscription: boolean
 }
 
-const UserProgress = ({ activeCourse, hearts, points, hasActiveSubscription }: Props) => {
+const UserProgress = ({
+  activeCourse,
+  hearts,
+  points,
+  hasActiveSubscription,
+}: Props) => {
   return (
     <div className="flex items-center justify-between gap-x-2 w-full">
       <Link href={'/courses'}>
@@ -26,14 +32,30 @@ const UserProgress = ({ activeCourse, hearts, points, hasActiveSubscription }: P
       </Link>
       <Link href={'/shop'}>
         <Button variant={'ghost'} className=" text-orange-500">
-          <Image src={'/points.svg'} alt={'Points'} className="mr-2" width={28} height={28} />
+          <Image
+            src={'/points.svg'}
+            alt={'Points'}
+            className="mr-2"
+            width={28}
+            height={28}
+          />
           {points}
         </Button>
       </Link>
       <Link href={'/shop'}>
         <Button variant={'ghost'} className=" text-rose-500">
-          <Image src={'/heart.svg'} alt={'Hearts'} className="mr-2" width={22} height={22} />
-          {hasActiveSubscription ? <InfinityIcon className="h-4 w-4 stroke-[3]" /> : hearts}
+          <Image
+            src={'/heart.svg'}
+            alt={'Hearts'}
+            className="mr-2"
+            width={22}
+            height={22}
+          />
+          {hasActiveSubscription ? (
+            <InfinityIcon className="h-4 w-4 stroke-[3]" />
+          ) : (
+            hearts
+          )}
         </Button>
       </Link>
     </div>
--- db/queries.ts
+++ db/queries.ts
@@ -36,13 +36,15 @@ export const getUnits = cache(async () => {
     return []
   }
 
-  // TODO: Confirm whether order is needed
   const data = await db.query.units.findMany({
+    orderBy: (units, { asc }) => [asc(units.order)],
     where: eq(units.courseId, userProgress.activeCourseId),
     with: {
       lessons: {
+        orderBy: (lessons, { asc }) => [asc(lessons.order)],
         with: {
           challenges: {
+            orderBy: (challenges, { asc }) => [asc(challenges.order)],
             with: {
               challengeProgress: {
                 where: eq(challengeProgress.userId, userId),
@@ -94,7 +96,16 @@ export const getCourses = cache(async () => {
 export const getCourseById = cache(async (courseId: number) => {
   const data = await db.query.courses.findFirst({
     where: eq(courses.id, courseId),
-    // TODO: Populate units and lessons
+    with: {
+      units: {
+        orderBy: (units, { asc }) => [asc(units.order)],
+        with: {
+          lessons: {
+            orderBy: (lessons, { asc }) => [asc(lessons.order)],
+          },
+        },
+      },
+    },
   })
 
   return data
@@ -131,7 +142,6 @@ export const getCourseProgress = cache(async () => {
   const firstUncompletedLesson = unitsInActiveCourse
     .flatMap((unit) => unit.lessons)
     .find((lesson) => {
-      // TODO: If something does not work, check the last if clause
       return lesson.challenges.some((challenge) => {
         return (
           !challenge.challengeProgress ||
@@ -246,4 +256,24 @@ export const getUserSubscription = cache(async () => {
     ...data,
     isActive: !!isActive,
   }
+})
+
+export const getTopTenUsers = cache(async () => {
+  const { userId } = await auth()
+  if (!userId) {
+    return []
+  }
+
+  const data = await db.query.userProgress.findMany({
+    orderBy: (userProgress, { desc }) => [desc(userProgress.points)],
+    limit: 10,
+    columns: {
+      userId: true,
+      userName: true,
+      userImageSrc: true,
+      points: true,
+    },
+  })
+
+  return data
 })
